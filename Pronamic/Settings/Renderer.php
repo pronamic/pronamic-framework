11e667e37afd81b3042021f190ae4381df7aed4b
Leon Rowland <leon@rowland.nl>
2013-02-01 14:43:53 +0100

    Addition of using upload helper

--- Pronamic/Settings/Renderer.php
+++ Pronamic/Settings/Renderer.php
@@ -32,70 +32,24 @@ class Pronamic_Settings_Renderer {
         );
     }
 
-    public function editor( $args )
-    {
+    public function editor( $args ) {
         wp_editor( 
             get_option( $args['label_for'] ), 
             $args['label_for'] 
         );
     }
 
-    public function upload( $args )
-    {
-        wp_enqueue_media();
+    public function upload( $args ) {
+        echo Pronamic_Helper_Upload::scripts();
 
-        $input_text = Pronamic_Helper_Html::text(
+        echo Pronamic_Helper_Upload::inputs(
             $args['label_for'],
             $args['label_for'],
-            get_option( $args['label_for'] ),
-            array( 'widefat' )
+            get_option( $args['label_for'] )
         );
-
-        $input_button = Pronamic_Helper_Html::button(
-            $args['label_for'] . '_button',
-            $args['label_for'] . '_button',
-            __( 'Upload' ),
-            array( 'button', 'button-secondary', 'jMediaUploader' )
-        );
-
-        echo "<div class='uploader'>";
-        echo $input_text;
-        echo $input_button;
-        echo "</div>";
-
-        echo "
-            <script type='text/javascript'>
-                jQuery(document).ready(function($){
-                    var PF_Upload = {
-                        button: '',
-                        ready: function() {
-                            PF_Upload.button = $('.jMediaUploader');
-                            PF_Upload.binds();
-                        },
-                        binds: function() {
-                            PF_Upload.button.click(PF_Upload.do_upload);
-                        },
-                        do_upload: function(e) {
-                            e.preventDefault();
-                            var self = $(this),
-                                input = self.attr('id').replace('_button', '');
-
-                            wp.media.editor.send.attachment = function(props, attachment) {
-                                $('#' + input).val(attachment.url);
-                            };
-
-                            wp.media.editor.open(self);
-                        }
-                    };
-
-                    PF_Upload.ready();
-                });
-            </script>
-        ";
     }
 
-    public function colorpicker( $args )
-    {
+    public function colorpicker( $args ) {
         wp_enqueue_script( 'wp-color-picker');
         wp_enqueue_style( 'wp-color-picker' );
 
@@ -125,5 +79,38 @@ class Pronamic_Settings_Renderer {
                 });
             </script>
         ";
+    }
+    
+    public function radio( $args ) {
+        
+        $values = array();
+        if ( ! empty( $args['values'] ) ) {
+            foreach( $args['values'] as $value ) {
+                echo Pronamic_Helper_Html::radio(
+                    $args['label_for'],
+                    $args['label_for'],
+                    $value,
+                    get_option( $args['label_for'] )
+                );
+            }
+        }
+    }
+    
+    public function password( $args ) {
+        echo Pronamic_Helper_Html::password( 
+            $args['label_for'], 
+            $args['label_for'], 
+            null, 
+            array('regular-text', 'code') 
+        );
+    }
+    
+    public function hidden( $args ) {
+        echo Pronamic_Helper_Html::hidden( 
+            $args['label_for'], 
+            $args['label_for'], 
+            get_option( $args['label_for'] ), 
+            array('regular-text', 'code') 
+        );
     }
 }
